005dd969f2977760a2a7a641e8a2d211264d29be
Kastan Day <kastanvday@gmail.com>
2023-07-30 14:01:05 -0700

    restructure ML for Bio project

--- ML_for_Bio/outer_loop_planner.py
+++ ML_for_Bio/outer_loop_planner.py
@@ -1,3 +1,13 @@
+'''
+
+## USAGE
+
+In Notebooks, since they have their own eventloop:
+  import nest_asyncio
+  nest_asyncio.apply()
+'''
+
+
 import asyncio
 import os
 
@@ -15,90 +25,13 @@ from langchain.tools.playwright.utils import \
 from langchain.tools.playwright.utils import create_async_playwright_browser
 from llama_hub.github_repo import GithubClient, GithubRepositoryReader
 
+from ML_for_Bio.tools import get_docstore_agent, get_shell_tool
+
 load_dotenv(override=True, dotenv_path='../.env')
 
 os.environ["LANGCHAIN_TRACING"] = "true" # If you want to trace the execution of the program, set to "true"
 langchain.debug = True
 VERBOSE = True
-
-# This import is required only for jupyter notebooks, since they have their own eventloop
-# import nest_asyncio
-# nest_asyncio.apply()
-
-def get_docstore_agent(docstore: Docstore | None = None):
-  """This returns an agent. Usage of this agent: react.run(question)"""
-  if docstore is None:
-    doc_explorer = DocstoreExplorer(langchain.Wikipedia())
-  else:
-    doc_explorer = DocstoreExplorer(docstore)
-  
-  tools = [
-      Tool(
-          name="Search",
-          func=doc_explorer.search,
-          description="useful for when you need to ask with search",
-      ),
-      Tool(
-          name="Lookup",
-          func=doc_explorer.lookup,
-          description="useful for when you need to ask with lookup",
-      ),
-  ]
-
-  llm = ChatOpenAI(temperature=0, model="gpt-4-0613", max_retries=3, request_timeout=60 * 3) # type: ignore
-  react = initialize_agent(tools, llm, agent=AgentType.REACT_DOCSTORE, verbose=VERBOSE)
-  return react
-
-from langchain.callbacks import HumanApprovalCallbackHandler
-from langchain.tools import ShellTool
-
-
-def _should_check(serialized_obj: dict) -> bool:
-    # Only require approval on ShellTool.
-    return serialized_obj.get("name") == "terminal"
-
-
-def _approve(_input: str) -> bool:
-    if _input == "echo 'Hello World'":
-        return True
-    msg = (
-        "Do you approve of the following input? "
-        "Anything except 'Y'/'Yes' (case-insensitive) will be treated as a no."
-    )
-    msg += "\n\n" + _input + "\n"
-    resp = input(msg)
-    return resp.lower() in ("yes", "y")
-
-def get_shell_tool():
-  '''Adding the default HumanApprovalCallbackHandler to the tool will make it so that a user has to manually approve every input to the tool before the command is actually executed.
-  Human approval on certain tools only: https://python.langchain.com/docs/modules/agents/tools/human_approval#configuring-human-approval
-  '''
-  tool = ShellTool(callbacks=[HumanApprovalCallbackHandler(should_check=_should_check, approve=_approve)])
-
-
-def get_tools(llm):
-  # TOOLS
-  async_browser = create_async_playwright_browser()
-  browser_toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
-  # sync_browser = create_sync_playwright_browser()
-  # browser_toolkit = PlayWrightBrowserToolkit.from_browser(sync_browser=sync_browser)
-  browser_tools = browser_toolkit.get_tools()
-
-  human_tools = load_tools(
-      ["human"],
-      llm=llm,
-      input_func=get_human_input
-  )
-  
-  # Agent to search docs and pull out helpful sections, code & examples
-  get_docstore_agent()
-  
-  python = get_shell_tool()
-  
-  
-  
-  tools = browser_tools + human_tools
-  return tools
 
 def get_human_input() -> str:
   """Placeholder for Slack input from user."""
