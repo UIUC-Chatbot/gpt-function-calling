81392ec5b04ecff4236dae2384d63c6a02ebc810
Kastan Day <kastanvday@gmail.com>
2023-07-31 00:41:12 -0700

    working AutoGPT inside Langhcain. Very useful to have all those functions in this ecosystem

--- ML_for_Bio/outer_loop_planner.py
+++ ML_for_Bio/outer_loop_planner.py
@@ -12,7 +12,7 @@ On the command line
 import asyncio
 import os
 import threading
-from typing import Sequence
+from typing import List, Sequence
 
 import langchain
 from agents import get_docstore_agent
@@ -20,8 +20,11 @@ from dotenv import load_dotenv
 from langchain.agents import AgentType, Tool, initialize_agent, load_tools
 from langchain.agents.agent_toolkits import PlayWrightBrowserToolkit
 from langchain.agents.react.base import DocstoreExplorer
+from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+from langchain.chat_models.base import BaseChatModel
 from langchain.docstore.base import Docstore
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import MessagesPlaceholder
 from langchain.schema.language_model import BaseLanguageModel
@@ -29,23 +32,65 @@ from langchain.tools.base import BaseTool
 from langchain.tools.playwright.utils import \
     create_sync_playwright_browser  # A synchronous browser is available, though it isn't compatible with jupyter.
 from langchain.tools.playwright.utils import create_async_playwright_browser
+from langchain.vectorstores import Qdrant
 from llama_hub.github_repo import GithubClient, GithubRepositoryReader
+from qdrant_client import QdrantClient
 from tools import get_shell_tool, get_tools
 
-load_dotenv(override=True, dotenv_path='../.env')
+load_dotenv(override=True, dotenv_path='.env')
 
 os.environ["LANGCHAIN_TRACING"] = "true"  # If you want to trace the execution of the program, set to "true"
 langchain.debug = True
 VERBOSE = True
 
+from langchain_experimental.autonomous_agents.autogpt.agent import AutoGPT
+from langchain_experimental.autonomous_agents.baby_agi import BabyAGI
 from langchain_experimental.plan_and_execute.agent_executor import \
     PlanAndExecute
 from langchain_experimental.plan_and_execute.executors.agent_executor import \
     load_agent_executor
-# Load an agent executor.
 from langchain_experimental.plan_and_execute.planners.chat_planner import \
     load_chat_planner
 
+
+class OuterLoopPlanner:
+  
+  def __init__(self):
+    print("Before qdrant")
+    self.qdrant_client = QdrantClient(
+          url=os.getenv('QDRANT_URL'),
+          api_key=os.getenv('QDRANT_API_KEY'))
+
+    print("Before creating vectorstore")
+    self.langchain_docs_vectorstore = Qdrant(
+        client=self.qdrant_client,
+        collection_name=os.getenv('QDRANT_LANGCHAIN_DOCS'),  # type: ignore
+        embeddings=OpenAIEmbeddings())  # type: ignore
+
+    print("Before creating vectorstore")
+    self.langchain_docs_vectorstore = Qdrant(
+        client=self.qdrant_client,
+        collection_name=os.getenv('QDRANT_LANGCHAIN_DOCS'),  # type: ignore
+        embeddings=OpenAIEmbeddings())  # type: ignore
+  
+    print("after __init__")
+    # todo: try babyagi
+    # BabyAGI()
+    
+  def autogpt_babyagi(self, llm: BaseChatModel, tools: List[BaseTool], prompt: str, memory=None):
+    
+    # langchain_retriever = RetrievalQA.from_chain_type(llm=ChatOpenAI(model='gpt-4-0613'), chain_type="stuff", retriever=self.langchain_docs_vectorstore.as_retriever())
+    
+    autogpt = AutoGPT.from_llm_and_tools(
+      ai_name='coding assistant',
+      ai_role='ML engineer',
+      tools=tools,
+      llm=llm,
+      human_in_the_loop=True,
+      memory=self.langchain_docs_vectorstore.as_retriever(),
+    )
+    goals: List[str] = [prompt]
+    autogpt.run(goals=goals)
 
 def experimental_main(llm: BaseLanguageModel, tools: Sequence[BaseTool], memory, prompt: str):
   input = {'input': prompt}
@@ -77,8 +122,8 @@ def experimental_main(llm: BaseLanguageModel, tools: Sequence[BaseTool], memory,
 
 def main(llm: BaseLanguageModel, tools: Sequence[BaseTool], memory, prompt: str):
   agent_chain = initialize_agent(
-      tools,
-      llm,
+      tools=tools,
+      llm=llm,
       agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
       verbose=VERBOSE,
       handle_parsing_errors=True,  # or pass a function that accepts the error and returns a string
@@ -141,7 +186,15 @@ if __name__ == "__main__":
   # prompt = "Browse to https://lumetta.web.engr.illinois.edu/120-binary/120-binary.html and complete the first challenge using the keyboard to enter information, please."
 
   # prompt = "Please find the Python docs for LangChain, then write a function that takes a list of strings and returns a list of the lengths of those strings."
-  prompt = "Please find the Python docs for LangChain to help you write an example of a Retrieval QA chain, or anything like that which can answer questions against a corpus of documents. Return just a single example in Python, please."
+  # prompt = "Please find the Python docs for LangChain to help you write an example of a Retrieval QA chain, or anything like that which can answer questions against a corpus of documents. Return just a single example in Python, please."
+  # ✅ worked with both Langchain PlanExecuteAgents. But NOT AutoGPT because it struggled with the internet.
+  prompt = "Write an example of making parallel requests to an API with expentional backoff, or anything like that. Return just a single example in Python, please."
+  prompt = "Write an example of making parallel requests to an API with expentional backoff, or anything like that. Return just a single example in Python, please."
 
   # main(llm=llm, tools=tools, memory=memory, prompt=prompt)
-  experimental_main(llm=llm, tools=tools, memory=memory, prompt=prompt)
+  # experimental_main(llm=llm, tools=tools, memory=memory, prompt=prompt)
+  # vectorstore 
+  
+  
+  o = OuterLoopPlanner()
+  o.autogpt_babyagi(llm=llm, tools=tools, prompt=prompt)
